08b70dc38fd2c8e204851a3ba62b7f5756e44bd2
Canjas <71513652+BlaisMathieu@users.noreply.github.com>
2021-11-09 16:04:06 +0100

    Update EventListeners.java

--- ChunkPopulator/src/Canjas/EventListeners.java
+++ ChunkPopulator/src/Canjas/EventListeners.java
@@ -12,8 +12,8 @@ import org.bukkit.event.world.WorldInitEvent;
 public class EventListeners implements Listener {
     @EventHandler
     public void onInit(WorldInitEvent event) {
-        event.getWorld().getPopulators().add(new ChunkPopulatorListenerBigVein(Material.DIAMOND_ORE, 3200));
-        event.getWorld().getPopulators().add(new ChunkPopulatorListenerBigVein(Material.GOLD_ORE, 1700));
+        event.getWorld().getPopulators().add(new ChunkPopulatorListenerBigVein(Material.DIAMOND_ORE, 4500));
+        event.getWorld().getPopulators().add(new ChunkPopulatorListenerBigVein(Material.GOLD_ORE, 3000));
         event.getWorld().getPopulators().add(new ChunkPopulatorListenerSmallVein(Material.LAPIS_ORE, 3000));
         event.getWorld().getPopulators().add(new ChunkPopulatorListenerSmallVein(Material.OBSIDIAN, 4000));
     }
